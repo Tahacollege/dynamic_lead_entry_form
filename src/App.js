1db24335cfe639ead2af6a1fa090fb2f2c18b34b
Taha <110016145+Tahacollege@users.noreply.github.com>
2024-09-28 21:22:39 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import axios from 'axios'
 function App() {
+  const [serverdata,setdata]=React.useState([]);
+  const [form,setform]=React.useState({});
+  const connect=async()=>{
+    const res=await axios.get('http://localhost:4000/data');
+    setdata(res.data);
+  }
+  React.useEffect(()=>{
+    connect();
+  },[])
+
+  const datastore=(e)=>{
+    setform({
+      ...form,
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const submited=async(e)=>{
+    e.preventDefault()
+    const name=form.Full_Name
+    const email=form.Email_Address
+    const tel=form.Contact_Number
+    const select=form.Lead_Type
+    const companyname=form.Company_Name
+    const file=form.Document
+    const res=await axios.post('http://localhost:4000/submit',{
+      name,
+      email,
+      tel,
+      select,
+      companyname,
+      file
+    })
+    //alert(JSON.stringify(form))
+
+  }
+
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {serverdata.map((item)=>
+        <div>
+          
+          <center>
+          <form className="form" onSubmit={submited}>
+          <h1>{item.title}</h1>
+          <hr />
+          <br />
+          {(item.fields).map((field_item)=>
+          <div>
+            <b><label >{field_item.label}: </label></b>
+              {field_item.type==='select' || field_item.type==='checkbox' || field_item.type==='radio'?<select name={field_item.label} className='input' onChange={datastore}>
+                {(field_item.options).map((options)=>
+                <option  value={options}>{options}</option>)}
+              </select>
+              :
+              <input className='input' name={field_item.label} type={field_item.type} required={field_item.required} onChange={datastore}></input>}
+              
+                
+              
+              
+            </div>
+          )}
+          <button className="btn">Submit</button>
+          </form>
+          </center>
+        </div>
+        )}
+
     </div>
   );
 }
